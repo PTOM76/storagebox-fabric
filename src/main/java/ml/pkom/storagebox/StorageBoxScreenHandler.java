8e8f64212acf570c545ff63162874570456463c7
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-02-06 17:27:09 +0900

    backport 1.19.3

--- src/main/java/ml/pkom/storagebox/StorageBoxScreenHandler.java
+++ src/main/java/ml/pkom/storagebox/StorageBoxScreenHandler.java
@@ -13,7 +13,7 @@ public class StorageBoxScreenHandler extends ScreenHandler {
     public static ScreenHandlerType<StorageBoxScreenHandler> SCREEN_HANDLER_TYPE;
 
     public static void init() {
-        SCREEN_HANDLER_TYPE = Registry.register(Registries.SCREEN_HANDLER,  StorageBoxMod.id("storagebox"), new ScreenHandlerType<>(StorageBoxScreenHandler::new, FeatureSet.empty()));
+        SCREEN_HANDLER_TYPE = Registry.register(Registries.SCREEN_HANDLER,  StorageBoxMod.id("storagebox"), new ScreenHandlerType<>(StorageBoxScreenHandler::new));
     }
     //public static ScreenHandlerType<StorageBoxScreenHandler> SCREEN_HANDLER_TYPE = ScreenHandlerRegistry.registerSimple(StorageBoxMod.id("storagebox"), StorageBoxScreenHandler::new);
 

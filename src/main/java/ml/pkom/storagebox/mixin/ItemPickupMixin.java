e8e41eb24dca162343f05a87352a34168910668b
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-02-07 10:18:47 +0900

    support useOnBlock support NBT

--- src/main/java/ml/pkom/storagebox/mixin/ItemPickupMixin.java
+++ src/main/java/ml/pkom/storagebox/mixin/ItemPickupMixin.java
@@ -2,7 +2,6 @@ package ml.pkom.storagebox.mixin;
 
 import ml.pkom.storagebox.ModConfig;
 import ml.pkom.storagebox.StorageBoxItem;
-import ml.pkom.storagebox.StorageBoxSlot;
 import net.minecraft.block.ShulkerBoxBlock;
 import net.minecraft.block.entity.ShulkerBoxBlockEntity;
 import net.minecraft.entity.ItemEntity;
@@ -35,7 +34,7 @@ public class ItemPickupMixin {
             ItemStack stackInNbt = getStackInStorageBox(stack);
             if (stackInNbt == null) return false;
             if (stackInNbt.getItem() == pickupStack.getItem()) {
-                if (!StorageBoxSlot.canInsertStack(pickupStack)) return false;
+                if (!StorageBoxItem.canInsertStack(pickupStack, stack)) return false;
                 setItemStackSize(stack, getItemDataAsInt(stack, KEY_SIZE) + pickupStack.getCount());
                 return true;
             }

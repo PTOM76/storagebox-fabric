184c369878287b691142cd864959468c38218641
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2025-08-03 00:20:40 +0900

    fix: refactor key handling and restore functionality for storage box interactions

--- src/main/java/net/pitan76/storagebox/StorageBoxClient.java
+++ src/main/java/net/pitan76/storagebox/StorageBoxClient.java
@@ -1,10 +1,14 @@
 package net.pitan76.storagebox;
 
+import net.legacyfabric.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.legacyfabric.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.MinecraftClient;
+import net.legacyfabric.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.legacyfabric.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.client.option.KeyBinding;
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.render.model.ModelLoader;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.PacketByteBuf;
+import net.pitan76.storagebox.mixin.KeyBindingAccessor;
 import org.lwjgl.input.Keyboard;
 
 public class StorageBoxClient implements ClientModInitializer {
@@ -19,7 +23,6 @@ public class StorageBoxClient implements ClientModInitializer {
                 "key.storagebox.category"
         ));
 
-        //ScreenProviderRegistry.INSTANCE.registerFactory(StorageBoxMod.id("container"), StorageBoxScreen.FACTORY);
 
 //        ColorProviderRegistry.ITEM.register(((storageBoxStack, tintIndex) -> {
 //            ItemStack stack = getStackInStorageBox(storageBoxStack);
@@ -37,74 +40,71 @@ public class StorageBoxClient implements ClientModInitializer {
 //
 //        }), StorageBoxItem.instance);
 
-//        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-//            if (isKeyPressed()) {
-//                PlayerEntity player = client.player;
-//                if (player == null) return;
-//                if (player.getMainHandStack() == null) return;
-//
-//                if (player.getMainHandStack().getItem() instanceof StorageBoxItem && player.getMainHandStack().hasNbt()) {
-//                    if (isKeyDownShift()) {
-//                        if (isKeyDownCtrl()) {
-//                            // ドロップ: (: + Shift + Ctrl)
-//                            PacketByteBuf buf = PacketByteBufs.create();
-//                            buf.writeString("put_out_and_throw");
-//                            ClientPlayNetworking.send(StorageBoxMod.lfid("key"), buf);
-//                        } else {
-//                            // 取り出す or コンテナーへ一括収納: (: + Shift)
-//                            PacketByteBuf buf = PacketByteBufs.create();
-//                            buf.writeString("put_out");
-//                            ClientPlayNetworking.send(StorageBoxMod.lfid("key"), buf);
-//                        }
-//
-//                    } else {
-//                        if (isKeyDownCtrl()) {
-//                            // AutoCollect切り替え: (: + Ctrl)
-//                            PacketByteBuf buf = PacketByteBufs.create();
-//                            buf.writeString("auto_collect");
-//                            ClientPlayNetworking.send(StorageBoxMod.lfid("key"), buf);
-//                        } else {
-//                            // コンテナーやインベントリからすべてストレージボックスへ一括収納: (:)
-//                            PacketByteBuf buf = PacketByteBufs.create();
-//                            buf.writeString("put_in");
-//                            ClientPlayNetworking.send(StorageBoxMod.lfid("key"), buf);
-//                        }
-//                    }
-//                }
-//            }
-//            if (client.player != null) {
-//                PlayerEntity player = client.player;
-//                if (player.getMainHandStack().getItem() instanceof StorageBoxItem)
-//                    StorageBoxItem.showBar(player, player.getMainHandStack());
-//            }
-//            coolDown--;
-//        });
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (isKeyPressed()) {
+                PlayerEntity player = client.player;
+                if (player == null) return;
+                if (player.getMainHandStack() == null) return;
+
+                if (player.getMainHandStack().getItem() instanceof StorageBoxItem && player.getMainHandStack().hasNbt()) {
+                    if (isKeyDownShift()) {
+                        if (isKeyDownCtrl()) {
+                            // ドロップ: (: + Shift + Ctrl)
+                            PacketByteBuf buf = PacketByteBufs.create();
+                            buf.writeString("put_out_and_throw");
+                            ClientPlayNetworking.send(StorageBoxMod.lfid("key"), buf);
+                        } else {
+                            // 取り出す or コンテナーへ一括収納: (: + Shift)
+                            PacketByteBuf buf = PacketByteBufs.create();
+                            buf.writeString("put_out");
+                            ClientPlayNetworking.send(StorageBoxMod.lfid("key"), buf);
+                        }
+
+                    } else {
+                        if (isKeyDownCtrl()) {
+                            // AutoCollect切り替え: (: + Ctrl)
+                            PacketByteBuf buf = PacketByteBufs.create();
+                            buf.writeString("auto_collect");
+                            ClientPlayNetworking.send(StorageBoxMod.lfid("key"), buf);
+                        } else {
+                            // コンテナーやインベントリからすべてストレージボックスへ一括収納: (:)
+                            PacketByteBuf buf = PacketByteBufs.create();
+                            buf.writeString("put_in");
+                            ClientPlayNetworking.send(StorageBoxMod.lfid("key"), buf);
+                        }
+                    }
+                }
+            }
+            if (client.player != null) {
+                PlayerEntity player = client.player;
+                if (player.getMainHandStack().getItem() instanceof StorageBoxItem)
+                    StorageBoxItem.showBar(player, player.getMainHandStack());
+            }
+            coolDown--;
+        });
     }
 
     private int coolDown = 0;
 
-//    private boolean isKeyPressed() {
-//        final Window mw = new Window(MinecraftClient.getInstance());
-//        if (InputUtil.isKeyPressed(mw.getHandle(), ((KeyBindingAccessor) keyBinding_COLON).getKeyCode().getKeyCode())) {
-//            if (coolDown <= 0) {
-//                coolDown = 3;
-//                return true;
-//            } else {
-//                return false;
-//            }
-//        }
-//        return false;
-//    }
-//
-//    private boolean isKeyDownShift() {
-//        final Window mw = MinecraftClient.getInstance().window;
-//        return InputUtil.isKeyPressed(mw.getHandle(), GLFW.GLFW_KEY_LEFT_SHIFT)
-//                || InputUtil.isKeyPressed(mw.getHandle(), GLFW.GLFW_KEY_RIGHT_SHIFT);
-//    }
-//
-//    private boolean isKeyDownCtrl() {
-//        final Window mw = MinecraftClient.getInstance().window;
-//        return InputUtil.isKeyPressed(mw.getHandle(), GLFW.GLFW_KEY_LEFT_CONTROL)
-//                || InputUtil.isKeyPressed(mw.getHandle(), GLFW.GLFW_KEY_RIGHT_CONTROL);
-//    }
+    private boolean isKeyPressed() {
+        if (Keyboard.isKeyDown(((KeyBindingAccessor) keyBinding_COLON).getCode())) {
+            if (coolDown <= 0) {
+                coolDown = 3;
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private boolean isKeyDownShift() {
+        return Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)
+                || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT);
+    }
+
+    private boolean isKeyDownCtrl() {
+        return Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)
+                || Keyboard.isKeyDown(Keyboard.KEY_RCONTROL);
+    }
 }
--- src/main/java/net/pitan76/storagebox/StorageBoxMod.java
+++ src/main/java/net/pitan76/storagebox/StorageBoxMod.java
@@ -3,7 +3,6 @@ package net.pitan76.storagebox;
 import net.fabricmc.api.ModInitializer;
 import net.legacyfabric.fabric.api.registry.v2.RegistryHelper;
 import net.legacyfabric.fabric.api.registry.v2.RegistryIds;
-import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
@@ -25,13 +24,6 @@ public class StorageBoxMod implements ModInitializer
         ModConfig.init();
     }
 
-    private int getNextAvailableItemId() {
-        int id = 0;
-        while (Item.REGISTRY.getByRawId(id) != null && id < 32000) {
-            id++;
-        }
-        return id;
-    }
     private static final Logger LOGGER = LogManager.getLogger();
 
     public static void log(Level level, String message){

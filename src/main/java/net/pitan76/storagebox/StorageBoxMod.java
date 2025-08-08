6860d13bbfa03ac90bd8760c840e636413e36cb4
PTOM76 <ptms76@outlook.jp>
2025-08-09 01:01:14 +0900

    remove import

--- src/main/java/net/pitan76/storagebox/StorageBoxMod.java
+++ src/main/java/net/pitan76/storagebox/StorageBoxMod.java
@@ -18,8 +18,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class StorageBoxMod implements ModInitializer
 {

652278511d06d6d9fd24fcd58360c4d078f0f027
PTOM76 <ptms76@outlook.jp>
2025-08-09 01:01:00 +0900

    fix: update storagebox recipe loading logic and refine JSON structure

--- src/main/java/net/pitan76/storagebox/StorageBoxMod.java
+++ src/main/java/net/pitan76/storagebox/StorageBoxMod.java
@@ -1,14 +1,25 @@
 package net.pitan76.storagebox;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import net.fabricmc.api.ModInitializer;
 import net.legacyfabric.fabric.api.registry.v2.RegistryHelper;
 import net.legacyfabric.fabric.api.registry.v2.RegistryIds;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.RecipeDispatcher;
+import net.minecraft.recipe.ShapedRecipeType;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class StorageBoxMod implements ModInitializer
 {
@@ -21,6 +32,37 @@ public class StorageBoxMod implements ModInitializer
         RegistryHelper.register(RegistryIds.ITEMS, lfid("storagebox"), StorageBoxItem.instance);
 
         RecipeDispatcher.method_14260(id("autocollectrecipes"), new AutoCollectRecipe(new ItemStack(StorageBoxItem.instance, 1, -1)));
+
+        Gson gson = new Gson();
+        JsonObject jsonObject;
+        try {
+            InputStream stream = getClass().getClassLoader().getResourceAsStream("assets/storagebox/recipes/storagebox.json");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
+
+            jsonObject = gson.fromJson(reader, JsonObject.class);
+            reader.close();
+        } catch (IOException | NullPointerException e) {
+            jsonObject = gson.fromJson("{\n" +
+                    "  \"type\": \"crafting_shaped\",\n" +
+                    "  \"pattern\": [\n" +
+                    "    \"XXX\",\n" +
+                    "    \"X X\",\n" +
+                    "    \"XXX\"\n" +
+                    "  ],\n" +
+                    "  \"key\": {\n" +
+                    "    \"X\": {\n" +
+                    "      \"item\": \"minecraft:chest\"\n" +
+                    "    }\n" +
+                    "  },\n" +
+                    "  \"result\": {\n" +
+                    "    \"item\": \"storagebox:storagebox\",\n" +
+                    "    \"count\": 1\n" +
+                    "  }\n" +
+                    "}", JsonObject.class);
+        }
+
+        if (jsonObject != null)
+            RecipeDispatcher.method_14260(id("storagebox"), ShapedRecipeType.load(jsonObject));
 
         StorageBoxScreenHandler.init();
         StorageBoxServer.init();

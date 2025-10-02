4886727246376c0b62eafc5e5b2dbf7d086f5072
Minor2CCh <minormugen716@gmail.com>
2025-10-02 19:49:41 +0900

    Fix Item Multiply bug.

--- src/main/java/net/pitan76/storagebox/StorageBoxSlot.java
+++ src/main/java/net/pitan76/storagebox/StorageBoxSlot.java
@@ -1,6 +1,5 @@
 package net.pitan76.storagebox;
 
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.slot.Slot;
@@ -9,12 +8,11 @@ import static net.pitan76.storagebox.StorageBoxItem.*;
 
 public class StorageBoxSlot extends Slot {
 
-    private PlayerEntity player;
+    private final StorageBoxScreenHandler handler;
 
-    public StorageBoxSlot(Inventory inventory, int index, int x, int y, PlayerEntity player) {
+    public StorageBoxSlot(StorageBoxScreenHandler handler, Inventory inventory, int index, int x, int y) {
         super(inventory, index, x, y);
-        this.player = player;
-
+        this.handler = handler;
     }
 
     @Override
@@ -26,20 +24,20 @@ public class StorageBoxSlot extends Slot {
     public void setStack(ItemStack itemStack) {
         super.setStack(itemStack);
         if (itemStack.isEmpty()) {
-            ItemStack storageBoxStack = player.getMainHandStack();
+            ItemStack storageBoxStack = handler.getHandStack();
             removeComponent(storageBoxStack, DataComponentTypes.ITEM_COUNT);
             removeComponent(storageBoxStack, DataComponentTypes.ITEM_DATA);
             removeComponent(storageBoxStack, DataComponentTypes.AUTO_COLLECT);
             return;
         }
-        ItemStack storageBoxStack = player.getMainHandStack();
+        ItemStack storageBoxStack = handler.getHandStack();
         setItemStack(storageBoxStack, itemStack.copy());
         setItemStackSize(storageBoxStack, itemStack.getCount());
     }
 
     @Override
     public ItemStack takeStack(int amount) {
-        ItemStack storageBoxStack = player.getMainHandStack();
+        ItemStack storageBoxStack = handler.getHandStack();
         if (!(storageBoxStack.getItem() instanceof StorageBoxItem)) return super.takeStack(amount);
         if (amount == getStack().getCount()) {
             removeComponent(storageBoxStack, DataComponentTypes.ITEM_COUNT);

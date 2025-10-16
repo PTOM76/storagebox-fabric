2c2330d328cb6199218c2577a1480278e2707df3
Minor2CCh <minormugen716@gmail.com>
2025-10-16 21:12:41 +0900

    Fixed a bug where changes to a component of the same item were not reflected (exam.bundle, 1.21Only) Fixed a bug where the StorageBox count overflowed when exceeding 2147483647 Added the tag “storagebox:storagebox_blacklist” to control items that cannot be placed in the StorageBox (default is none)

--- src/main/java/net/pitan76/storagebox/mixin/ItemPickupMixin.java
+++ src/main/java/net/pitan76/storagebox/mixin/ItemPickupMixin.java
@@ -34,6 +34,7 @@ public class ItemPickupMixin {
             if (stackInNbt == null) return false;
             if (stackInNbt.getItem() == pickupStack.getItem()) {
                 if (!StorageBoxItem.canInsertStack(pickupStack, stack)) return false;
+                if (getItemDataAsInt(stack, StorageBoxItem.KEY_SIZE) + pickupStack.getCount() < 0) return false; // 2147483647を超える場合は回収しない
                 StorageBoxItem.setItemStackSize(stack, StorageBoxItem.getItemDataAsInt(stack, StorageBoxItem.KEY_SIZE) + pickupStack.getCount());
                 return true;
             }

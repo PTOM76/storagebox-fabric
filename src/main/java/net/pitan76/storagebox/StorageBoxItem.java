5a1f918520600361b9e700231921daf1e3c2b03d
Minor2CCh <minormugen716@gmail.com>
2025-10-16 20:57:37 +0900

    Fixed an issue where propagation via bundles was not working correctly.

--- src/main/java/net/pitan76/storagebox/StorageBoxItem.java
+++ src/main/java/net/pitan76/storagebox/StorageBoxItem.java
@@ -238,9 +238,10 @@ public class StorageBoxItem extends Item {
                 // バケツから液体バケツなどのサポート
                 // stack.setCount(itemInBoxCount - 1);  // ここで数を減らすとインベントリが埋まっている時にバンドルのようなアイテムが消えてしまう(1.21~1.21.1のみ)
                 if (result.getResult().equals(ActionResult.CONSUME) || result.getValue() != stack) {
-                    ItemStack copyNewStack = result.getValue().copy();
-                    // copyNewStack.setCount(1);
-                    user.getInventory().offerOrDrop(copyNewStack);
+                    if(!user.getInventory().insertStack(stack)){
+                        dropItemStack(user, stack);
+                    }
+                    stack.setCount(0);
                 }
             }
 
